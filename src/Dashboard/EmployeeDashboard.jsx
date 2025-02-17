608891870fe4219800bc59742d23419867522a59
Your_Name <Your_Email>
2025-02-17 19:17:33 +0545

    Fixed import path for Header component

--- src/Dashboard/EmployeeDashboard.jsx
+++ src/Dashboard/EmployeeDashboard.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import Header from '../Components/Header'
-import TaskListNumber from '../Components/TaskListNumber'
-import TaskList from '../TaskList/TaskList'
+import React from 'react';
+import Header from '../Components/Header';  // Correct the import path
+import TaskListNumber from '../Components/TaskListNumber';
+import TaskList from '../TaskList/TaskList';
 
-const EmployeeDashboard = ({data}) => {
- 
+const EmployeeDashboard = ({ data }) => {
   return (
     <div className='p-10 bg-[#1C1C1C] h-screen'>
- 
-    <Header data = {data}/>
-    <TaskListNumber data = {data} />
-    <TaskList data = {data}/>
+      <Header data={data} />
+      <TaskListNumber data={data} />
+      <TaskList data={data} />
     </div>
-  )
-}
+  );
+};
 
-export default EmployeeDashboard
+export default EmployeeDashboard;
